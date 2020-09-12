c4fb646398a70619009d47c5e0b581f37b990de2
Fwzia YaSser <50033184+Fwzia@users.noreply.github.com>
2020-09-12 18:01:13 +0200

    rarings and comments

--- blueprints/comments.py
+++ blueprints/comments.py
@@ -1,3 +1,37 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, make_response
+import models.comments_model as cm
 
 comments = Blueprint('comments', __name__)
+
+@comments.route('', methods=['POST'])
+def add_new_comment():
+    id = 1
+    uid = request.json['uid']
+    pid = request.json['pid']
+    time = request.json['time']
+    comment = request.json['comment']
+
+    new_comment = cm.add_comment(comments, time, pid, uid)
+    return make_response(jsonify(id), 201)
+
+
+@comments.route('<id>', methods=['PUT', 'DELETE'])
+def change_comment():
+    id = 1
+    comment = request.json['comment']
+
+    if request.method == 'PUT' :
+        edited_comment = cm.edit_comment(id, comment)
+        return make_response(jsonify(id), 200)
+
+    elif request.method == 'DELETE' :
+        #change user info
+        deleted_comment = cm.delete_comment(id)
+        return make_response(jsonify({'message': 'DELETED'}), 200)
+    
+
+
+
+
+
+
--- blueprints/ratings.py
+++ blueprints/ratings.py
@@ -1,3 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, make_response
+import models.ratings_model as rm
 
 ratings = Blueprint('ratings', __name__)
+
+@ratings.route('', methods=['POST'])
+def add_new_rating():
+    id = 1
+    uid = request.json['uid']
+    pid = request.json['pid']
+    rate = request.json['rate']
+
+    new_rate = rm.add_rate(rate, pid ,uid)
+    return make_response(jsonify(id), 201)
+
+@ratings.route('<id>', methods=['PUT'])
+def change_rating():
+    id = 1
+    rate = request.json['rate']
+
+    edited_rate = rm.edit_rate(id, rate)
+    return make_response(jsonify(id), 200)
+

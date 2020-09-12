1929e21fe9b8b02ad06946c5c09d458769d3e9fa
Mostafa Hegab <mostafakmr1412@gmail.com>
2020-09-12 20:34:45 +0200

    comments and ratings review

--- blueprints/comments.py
+++ blueprints/comments.py
@@ -5,28 +5,23 @@ comments = Blueprint('comments', __name__)
 
 @comments.route('', methods=['POST'])
 def add_new_comment():
-    id = 1
     uid = request.json['uid']
     pid = request.json['pid']
     time = request.json['time']
     comment = request.json['comment']
-
-    new_comment = cm.add_comment(comments, time, pid, uid)
-    return make_response(jsonify(id), 201)
+    cm.add_comment(comment, time, pid, uid)
+    return make_response(jsonify({'message': 'comment added'}), 201)
 
 
 @comments.route('<id>', methods=['PUT', 'DELETE'])
 def change_comment():
-    id = 1
-    comment = request.json['comment']
-
     if request.method == 'PUT' :
-        edited_comment = cm.edit_comment(id, comment)
-        return make_response(jsonify(id), 200)
+        comment = request.json['comment']
+        cm.edit_comment(id, comment)
+        return make_response(jsonify({'message': 'comment modified'}), 200)
 
     elif request.method == 'DELETE' :
-        #change user info
-        deleted_comment = cm.delete_comment(id)
+        cm.delete_comment(id)
         return make_response(jsonify({'message': 'DELETED'}), 200)
     
 
--- blueprints/ratings.py
+++ blueprints/ratings.py
@@ -5,19 +5,16 @@ ratings = Blueprint('ratings', __name__)
 
 @ratings.route('', methods=['POST'])
 def add_new_rating():
-    id = 1
-    uid = request.json['uid']
+    uid = 1
     pid = request.json['pid']
     rate = request.json['rate']
-
-    new_rate = rm.add_rate(rate, pid ,uid)
-    return make_response(jsonify(id), 201)
+    rm.add_rate(rate, pid ,uid)
+    return make_response(jsonify({'message': 'rated'}), 201)
 
 @ratings.route('<id>', methods=['PUT'])
 def change_rating():
-    id = 1
+    uid = 1
     rate = request.json['rate']
-
-    edited_rate = rm.edit_rate(id, rate)
-    return make_response(jsonify(id), 200)
+    rm.edit_rate(id, rate)
+    return make_response(jsonify({'message': 'updated'}), 200)
 

1558b24ac1b8a0a796f7cf796bd1b97e27210271
Yommna Mohammed‏ <yommnamohamedhalwa@gmail.com>
2021-04-13 16:08:57 +0200

    preprocessing

--- utils/sentiment_analyzer.py
+++ utils/sentiment_analyzer.py
@@ -1,14 +1,27 @@
 
 from joblib import load 
+import string
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 loaded_joblib_model = load(filename="utils/Sentiment_Analysis_unigram.joblib")
 feats = loaded_joblib_model.feature_names
 feats_len = len(feats)
 
 def sentiment_analyzer(sent):
-    sent = sent.split(" ")
+    lemmatizer = WordNetLemmatizer()
+    sent =sent.lower()
+    sent = sent.translate(str.maketrans('', '', string.punctuation))
+    filtered_sentence = [] 
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(sent) 
+    filtered_sentence = [w for w in word_tokens if not w in stop_words] 
+    lemmatized_output =[]
+    lemmatized_output = [lemmatizer.lemmatize(w) for w in filtered_sentence ]
+
     sent_features=[]
     sent_dict = {}
-    for word in sent:
+    for word in lemmatized_output:
         if not word in sent_dict:
             sent_dict[word] = 0
         sent_dict[word] = sent_dict[word] + 1
@@ -19,3 +32,5 @@ def sentiment_analyzer(sent):
             sent_features.append(sent_dict[feats[i]])
     joblib_y_preds = loaded_joblib_model.predict([sent_features])
     return joblib_y_preds[0]
+
+
